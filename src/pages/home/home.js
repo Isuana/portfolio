b3f3a23c119c9d770c1c95210dbd16241f1b68c3
Isuana <106450739+Isuana@users.noreply.github.com>
2022-12-08 16:36:25 +0100

    creating a homepage (#1)

---
+++ src/pages/home/home.js
@@ -0,0 +1,16 @@
+import './home.css';
+
+const Home = () => {
+  return (
+    <>
+      <div className='note' id="note_center">
+        <p>Witaj!</p>
+        <p>Na tej stronie możesz skorzystać z wypełnienia formularza,
+        wpisania wielu notatek oraz rozwiązać mini-quiz.</p>
+        <p>Zapraszam!</p>
+      </div>
+    </>
+  )
+};
+
+export default Home;
---
+++ src/pages/page.js
@@ -0,0 +1,37 @@
+import { Link, Outlet } from "react-router-dom";
+import './page.css';
+import logo from '../images/logo.png';
+
+const Page = () => {
+  return (
+    <>
+      <nav>
+        <Link to="/">
+          <img src={logo} alt='logo' className="logo-view" />
+        </Link>
+      </nav>
+      <nav id='nav_panel'>
+        <ul className="left-menu">
+          <li className="left-list">
+            MENU
+          </li>
+          <li className="left-list">
+            Form
+          </li>
+          <li className="left-list">
+            Notebook
+          </li>
+          <li className="left-list">
+            Quiz
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default Page;
