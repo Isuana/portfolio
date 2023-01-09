8d4ea1abc045e6c662e6bb5d4850cb300692dc35
Klaudia <klaudiad18@outlook.com>
2023-01-09 13:49:22 +0100

    Fix deploy script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Page from './pages/page';
 import Home from './pages/home/home';
@@ -9,13 +9,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Page />}>
           <Route index element={<Home />} />
-        </Route>
+        </Route>``
       </Routes>
-    </RouterProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
