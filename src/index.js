9326bb627ae17970c00f92918add69ae739498c9
Klaudia <klaudiad18@outlook.com>
2023-01-09 11:32:09 +0100

    Add deploy option to github

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider, Routes, Route } from "react-router-dom";
 import './index.css';
 import Page from './pages/page';
 import Home from './pages/home/home';
@@ -9,13 +9,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <RouterProvider>
       <Routes>
         <Route path="/" element={<Page />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </RouterProvider>
   </React.StrictMode>
 );
 
